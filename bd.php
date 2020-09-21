24429dc90c384b3b44d458e44d2be861439cc304
Rafael Antunes <rafaelantunesvieira@outlook.com.br>
2020-09-21 18:57:22 -0300

    Conexão com o banco de dados

--- bd.php
+++ bd.php
@@ -1,5 +1,17 @@
 <?php
-$ds = 'pgsql:host=177.44.67.234;port=1998;dbname=sistema;charset=utf8';
+// Inicia a sessão.
+session_start();
+
+/* Conectando com o banco de dados para cadastrar registros */
+
+$datasource = 'pgsql:host= 177.44.64.105; port=1998 ;dbname=sis_car_aluguel';
 $user = 'postgres';
-$pass = 'bd';
-$db = new PDO($ds, $user, $pass);
+$pass = 'bd123';
+$db = new PDO($datasource, $user, $pass);
+	
+$query = "SELECT listarclientes()";
+$stm = $db->prepare($query);
+$stm->execute();
+
+
+?>
