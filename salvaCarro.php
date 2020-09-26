53f08fcf235c48af025b9eddcf86f8c921fa1e70
fmgflavio <fmgflavio@gmail.com>
2020-09-26 17:02:04 -0300

    salvaCarro

--- salvaCarro.php
+++ salvaCarro.php
@@ -1,11 +1,11 @@
 <?php
-// Inicia a sessão.
+# Inicia a sessão.
 #session_start();
 
 include_once "bd.php";
 
 #Recebe parâmetros para inserção no banco:
-
+$flag = 0;
 $placaAutomovel = $_POST['placaAutomovel'];
 $corAutomovel = $_POST['corAutomovel'];
 $chassisAutomovel = $_POST['chassisAutomovel'];
@@ -19,60 +19,99 @@ $tipo_de_combustivelAutomovel = $_POST['tipo_de_combustivelAutomovel'];
 $renavamAutomovel = $_POST['renavamAutomovel'];
 $tipoAutomovel = $_POST['tipoAutomovel'];
 $transmissaoAutomovel = $_POST['transmissaoAutomovel'];
+#$fotosAutomovel = $_POST['fotosAutomovel'];
 $statusAutomovel = 1;
 
 # O banco de dados possui uma funcao para realizar as inserções:
 $query = "INSERT INTO automovel VALUES ('$placaAutomovel','$corAutomovel','$chassisAutomovel','$direcaoAutomovel','$ar_condicionadoAutomovel','$manutencaoAutomovel','$nro_de_portaAutomovel','$quilometragemAutomovel', '$transmissaoAutomovel', '$marcaAutomovel','$tipo_de_combustivelAutomovel','$renavamAutomovel','$statusAutomovel','$tipoAutomovel')";
-$cod = 1;
 $stm = $db->prepare($query); 
 
-if ($stm->execute()) {
-    $flag = 1;
+
+if ($stm->execute()) {   
     
-    /*
-    $lastIdProduto = $db -> lastInsertId();
+    print "<p>Foi mazomenu</p>";
+    #$lastIdProduto = $db->lastInsertId();
     
     # Insercao da imagem:
-    if (isset($_FILES['arquivo']['name']) && $_FILES['arquivo']['error'] == 0) {
+    /*
+    $hora = uniqid(time());
+    $OID = pg_locreate ($fotosAutomovel);
+    $HANDLE_BLOB = pg_loopen ( $fotosAutomovel, $OID, 'w' );
+    pg_lowrite ( $HANDLE_BLOB, $$hora);
+    pg_loclose ( $HANDLE_BLOB );
+    pg_exec ( $fotosAutomovel,
+        "INSERT INTO tbImoveis_Fotos ( idImovel, Foto )
+         VALUES ( '$hora', $HANDLE_BLOB ) "
+    );*/
+    #$ret = isset($_FILES['fotosAutomovel']);
+    print "<p>Rodô</p>";
+    print "<p> placaAutomovel = $placaAutomovel </p>";
 
-        $arquivo_tmp = $_FILES['arquivo']['tmp_name'];
-        $nome = $_FILES['arquivo']['name'];
+    
+    if (isset($_FILES['fotosAutomovel'])){  #/*&& $_FILES['fotosAutomovel']['size'] > 0*/) {
+        print "<p>Entrou aqui </p>";
 
-        // Pega a extensão
+        $tmpName = $_FILES['fotosAutomovel']['tmp_name'];
+
+        # Ler arquivo de imagem:
+        $fp = fopen($tmpName, 'r');
+        $data_img = fread($fp, filesize($tmpName));
+        fclose($fp);
+        
+        $query = "UPDATE automovel SET fotos = ? WHERE placa = ?";
+        $stm = $db -> prepare($query);
+        $stm->bindParam(1, $data_img, PDO::PARAM_LOB);
+        $stm->bindParam(2, $placaAutomovel);
+
+        if($stm->execute()){
+            print "<p>Foi um sucesso</p>";
+            $flag = $flag + 1;
+        }
+        else{
+            print "<p>Falha de novo</p>";
+        }
+    }
+
+
+
+        /*
+        $arquivo_tmp = $_FILES['fotosAutomovel']['tmp_name'];
+        $nome = $_FILES['fotosAutomovel']['name'];
+
+        # Pega a extensão
         $extensao = pathinfo($nome, PATHINFO_EXTENSION);
+        echo "$extensao";
 
-        // Converte a extensão para minúsculo
+        # Converte a extensão para minúsculo
         $extensao = strtolower($extensao);
 
-        // Somente imagens, .jpg;.jpeg;.gif;.png
-        // Aqui eu enfileiro as extensões permitidas e separo por ';'
-        // Isso serve apenas para eu poder pesquisar dentro desta String
+        # Somente imagens, .jpg;.jpeg;.gif;.png
+        # Aqui eu enfileiro as extensões permitidas e separo por ';'
+        # Isso serve apenas para eu poder pesquisar dentro desta String
         if (strstr('.jpg;.jpeg;.gif;.png;.jfif', $extensao)) {
-            // Cria um nome único para esta imagem
-            // Evita que duplique as imagens no servidor.
-            // Evita nomes com acentos, espaços e caracteres não alfanuméricos
+            echo "<p>Entrou na extensao<p>";
+            # Cria um nome único para esta imagem
+            # Evita que duplique as imagens no servidor.
+            # Evita nomes com acentos, espaços e caracteres não alfanuméricos
             $novoNome1 = uniqid(time()) . '.' . $extensao;
 
-            // Concatena a pasta com o nome
+            # Concatena a pasta com o nome
             $destino = 'fotos/' . $novoNome1;
 
-            // tenta mover o arquivo para o destino
+            # tenta mover o arquivo para o destino
             if (@move_uploaded_file($arquivo_tmp, $destino)) {
-                $query = "UPDATE automovel SET fotos= %s WHERE placa = %s";
+                $query = "UPDATE automovel SET fotos = '$novoNome1' WHERE placa = '$placaAutomovel'";
                 $stm = $db -> prepare($query);
-                $stm -> bindParam(1, $novoNome1);
-                $stm -> bindParam(2, $lastIdProduto);
                 if ($stm -> execute()) {
                     $flag = $flag + 1;
                 }
             }
         }
-    }
-    
-    */
+        */
+    #}
     # Verificar se deu tudo certo
-
-    # Se flag não vale zero, sinal que funcionou a inserção		
+    # Se flag não vale zero, sinal que funcionou a inserção
+    
     if ($flag > 0) {
         header("location:index.php");
     } else {
@@ -80,11 +119,13 @@ if ($stm->execute()) {
         #header("location:salvaCarro.php?error=salvaCarro");
 
     }
+    
 } 
 else {
     print "<p>Faiô</p>";
     #header("location:salvaCarro.php?error=salvaCarro");
 }
+
 
 
 ?>
